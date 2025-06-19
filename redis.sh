dab805208f4e2141fb94c304b1f494e97aea582e
bindu <davuluribindu995@gmail.com>
2025-06-19 10:46:45 +0530

    modified file

--- redis.sh
+++ redis.sh
@@ -1,33 +1,9 @@
 #!/bin/bash
 
-userid=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-logs_folder="/var/log/shellscript-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-log_file="$logs_folder/$SCRIPT_NAME.log"
-script_dir=$PWD
+source ./common.sh
+app_name=redis
 
-mkdir -p $logs_folder
-echo "script started executing at:$(date)" | tee -a $log_file
-
-if [ $userid -ne 0 ]
-then 
-     echo -e "$R error:: please run the script with root user $N" | tee -a $log_file
-     exit 1
-else
-    echo "you are running with root user" | tee -a $log_file
-fi
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e " $2 is ... $G success $N" | tee -a $log_file
-    else 
-        echo -e " $2 is ... $R failure $N" | tee -a $log_file
-    fi
-}
+check_root
 
 dnf module disable redis -y
 VALIDATE $? "disabling the redis"
