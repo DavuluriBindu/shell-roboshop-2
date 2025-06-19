c953b403f39db079f5753ca190b1931705313855
bindu <davuluribindu995@gmail.com>
2025-06-19 10:58:09 +0530

    modified file

--- cart.sh
+++ cart.sh
@@ -3,8 +3,8 @@
 source ./common.sh
 app_name=cart
 
-nodejs_setup
 check_root
 app_setup
+nodejs_setup
 systemd_setup
 
--- shipping.sh
+++ shipping.sh
@@ -1,74 +1,23 @@
 #!/bin/bash
 
-userid=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-logs_folder="/var/log/shellscript-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-log_file="$logs_folder/$SCRIPT_NAME.log"
-script_dir=$PWD
+source ./common.sh
+app_name=shipping
 
-mkdir -p $logs_folder
-echo "script started executing at:$(date)" | tee -a $log_file
-
-if [ $userid -ne 0 ]
-then 
-     echo -e "$R error:: please run the script with root user $N" | tee -a $log_file
-     exit 1
-else
-    echo "you are running with root user" | tee -a $log_file
-fi
-
-echo "enter the mysql password"
-read -s Mysql_root_pasw
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e " $2 is ... $G success $N" | tee -a $log_file
-    else 
-        echo -e " $2 is ... $R failure $N" | tee -a $log_file
-    fi
-}
+check_root
+app_setup
+systemd_setup
 
 dnf install maven -y
 VALIDATE $? "installing maven "
 
-id roboshop
-if [ $? -ne 0 ]
-then
- useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$log_file
- VALIDATE $? "creating a user"
-else 
- echo -e "system user roboshop is present"
-fi
 
-mkdir /app 
-VALIDATE $? "make an app directory"
-
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip 
-VALIDATE $? "download shipping file"
-
-rm -rf /app/*
-cd /app 
-unzip /tmp/shipping.zip
-VALIDATE $? "unzip the user file"
 
 mvn clean package 
 VALIDATE $? "packaging the shipping application"
 mv target/shipping-1.0.jar shipping.jar 
 VALIDATE $? "moving and renaming the jar file"
 
-cp $script_dir/shipping.service /etc/systemd/system/shipping.service
-VALIDATE $? "adding shipping service "
 
-systemctl daemon-reload
-VALIDATE $? "daemon reload "
-
-systemctl enable shipping 
-systemctl start shipping
-VALIDATE $? "starting the shipping process"
 
 dnf install mysql -y 
 VALIDATE $? "instal mysql into shipping"
