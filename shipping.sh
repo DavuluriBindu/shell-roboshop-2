e1929986519e2414c6cc8649d3aed4dd3f65ff7b
bindu <davuluribindu995@gmail.com>
2025-06-19 12:07:07 +0530

    modified file

--- shipping.sh
+++ shipping.sh
@@ -14,15 +14,13 @@ systemd_setup
 
 
 
-
-
 dnf install mysql -y 
 VALIDATE $? "instal mysql into shipping"
 
 mysql -h mysql.devops84.site -u root -p$Mysql_root_pasw -e 'use cities'
 if [ $? -ne 0 ]
 then 
-  mysql -h mysql.devops84.site -uroot -p$Mysql_root_pasw < /app/db/app-user.sql 
+  mysql -h mysql.devops84.site -uroot -p$Mysql_root_pasw < /app/db/schema.sql.sql 
   mysql -h mysql.devops84.site -uroot -p$Mysql_root_pasw < /app/db/app-user.sql 
   mysql -h mysql.devops84.site -uroot -p$Mysql_root_pasw  < /app/db/master-data.sql
  VALIDATE $? "loading data to mysql"
