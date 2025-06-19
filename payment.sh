c1584402ebbcef7d1d72344d1077d556f749a12b
bindu <davuluribindu995@gmail.com>
2025-06-19 12:35:32 +0530

    modified file

--- payment.sh
+++ payment.sh
@@ -5,13 +5,9 @@ app_name=payment
 
 check_root
 app_setup
+python_setup
 systemd_setup
 
-dnf install python3 gcc python3-devel -y &>>$log_file
-VALIDATE $? "installing the python "
 
-
-pip3 install -r requirements.txt &>>$log_file
-VALIDATE $? "installing dependencies"
 
 
--- shipping.sh
+++ shipping.sh
@@ -20,7 +20,7 @@ VALIDATE $? "instal mysql into shipping"
 mysql -h mysql.devops84.site -u root -p$Mysql_root_pasw -e 'use cities'
 if [ $? -ne 0 ]
 then 
-  mysql -h mysql.devops84.site -uroot -p$Mysql_root_pasw < /app/db/schema.sql.sql 
+  mysql -h mysql.devops84.site -uroot -p$Mysql_root_pasw < /app/db/schema.sql
   mysql -h mysql.devops84.site -uroot -p$Mysql_root_pasw < /app/db/app-user.sql 
   mysql -h mysql.devops84.site -uroot -p$Mysql_root_pasw  < /app/db/master-data.sql
  VALIDATE $? "loading data to mysql"
